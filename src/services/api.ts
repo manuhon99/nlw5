3463febbba798666086463c3f9c0338b460eae68
manuhon99 <manuhon99@gmail.com>
2021-05-08 08:56:48 -0300

    json server

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333'|| 'https://my-json-server.typicode.com/magaliais/podcastr'
+  baseURL: 'http://localhost:3333'|| 'https://my-json-server.typicode.com/manuhon99/nlw5'
 })
